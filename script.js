1f2d30936aba767d76ac47f21266b568d75ad700
Bmsm230 <bryce.smith12@uky.edu>
2024-12-26 12:34:01 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Medications object
+const medications = {
+    "Normal Saline": ["0.9% 500 mL", "0.9% 1000 mL", "0.45% 500 mL", "0.45% 1000 mL"],
+    "Dextrose": ["5% 250 mL", "5% 500 mL", "5% 1000 mL", "10% 500 mL", "10% 1000 mL"],
+    // Other medication data omitted for brevity
+};
